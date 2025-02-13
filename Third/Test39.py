656ac7d1cf78232cc8ddb938b62424a73e7ab502
Dylanforwhat <qiuweili2021@gmail.com>
2025-02-14 00:12:22 +0900

    Numpy

--- Third/Test39.py
+++ Third/Test39.py
@@ -5,5 +5,12 @@ arr = [
     [4,5,6],
     [7,8,9]
 ]
-a = pd.DataFrame(arr,columns = list('xyz'),index = list('123'))
-print(a)
+a = pd.DataFrame(arr,columns = list('xyz'),index = list('abc'))
+print(a.apply(np.square)) # 全表平方根
+
+b = a.apply(lambda m:np.square(m) if m.name == 'x' else m) # 单列平方根
+c = a.apply(lambda m:np.square(m) if m.name in ['x','y'] else m) # 多列平方根
+d = a.apply(lambda m:np.square(m) if m.name == 'a' else m,axis = 1) # 单行平方根
+print(b)
+print(c)
+print(d)
---
+++ Third/Test40.py
@@ -0,0 +1,4 @@
+import pandas as pd
+a = "gender == 'male' and socre >= 150"
+
+print(数据.query(a))  # query 支持and
