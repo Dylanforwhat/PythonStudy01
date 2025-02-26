12fb5f8d7ecc1c55e41166cf0831e079aa07d3d3
Dylanforwhat <qiuweili2021@gmail.com>
2025-02-26 16:20:55 +0900

    Numpy

--- Third/Test62.py
+++ Third/Test62.py
@@ -4,3 +4,6 @@ b = pd.read_excel(a,'Sheet1')
 上月 = b.销售金额.shift()
 环比 = b.销售金额 - 上月
 b['环比增长'] = 环比
+
+years = b['日期'].dt.year
+c = pd.pivot_table(b.index = '店号',values = '金额',columns = 年,aggfunc = 'sum')
