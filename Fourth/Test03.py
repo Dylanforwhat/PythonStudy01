ef3e03487f1a662a5b1fe41105e60075c2078b11
Dylanforwhat <qiuweili2021@gmail.com>
2025-02-28 23:06:34 +0900

    Numpy

--- Fourth/Test03.py
+++ Fourth/Test03.py
@@ -5,4 +5,7 @@ os.environ['PYSPARK_PYTHON'] = "D:/python/python3.12.6/python.exe" #配置python
 conf = SparkConf().setMaster("local[*]").setAppName("test_spark")
 sc = SparkContext(conf = conf)
 
-rdd =
+rdd = sc.parallelize(["itheima itcast 666","itheima itheima itcast"])
+
+rdd2 = rdd.flatMap(lambda x:x.split(" ")) #flatMap 就是解除嵌套的意思
+print(rdd2.collect())
---
+++ Fourth/Test04.py
@@ -0,0 +1,13 @@
+from pyspark import SparkConf,SparkContext
+import os
+os.environ['PYSPARK_PYTHON'] = "D:/python/python3.12.6/python.exe" #配置python解释器的环境变量，供spark调用
+
+conf = SparkConf().setMaster("local[*]").setAppName("test_spark")
+sc = SparkContext(conf = conf)
+
+# reduceByKey 方法
+# 针对KV型RDD，自动按照key分组，然后根据你提供的聚合逻辑，完成组内数据的聚合操作
+# kv就是二元元组  只有两个元素的元组
+# rdd.reduceByKey(func)
+#  func:(K,V) → V  接受2个传入参数，类型一致，返回一个返回值
+
